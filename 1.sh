929af5cfee670ab3eddc672389046cd3568d8859
mhma <masn@bu.edu>
2024-12-31 15:09:24 +0800

    update save_path

--- 1.sh
+++ 1.sh
@@ -1,43 +1,43 @@
-python3 -m accelerate.commands.launch \
-    --num_processes=4 \
-    -m lmms_eval \
-    --model llava_onevision_with_kvcache \
-    --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.01 \
-    --tasks chartqa  \
-    --batch_size 1 \
-    --log_samples \
-    --log_samples_suffix chartqa \
-    --output_path ./logs/chartqa/
-python3 -m accelerate.commands.launch \
-    --num_processes=4 \
-    -m lmms_eval \
-    --model llava_onevision_with_kvcache \
-    --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.05 \
-    --tasks chartqa  \
-    --batch_size 1 \
-    --log_samples \
-    --log_samples_suffix chartqa \
-    --output_path ./logs/chartqa/
-python3 -m accelerate.commands.launch \
-    --num_processes=4 \
-    -m lmms_eval \
-    --model llava_onevision_with_kvcache \
-    --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.1 \
-    --tasks chartqa  \
-    --batch_size 1 \
-    --log_samples \
-    --log_samples_suffix chartqa \
-    --output_path ./logs/chartqa/
-python3 -m accelerate.commands.launch \
-    --num_processes=4 \
-    -m lmms_eval \
-    --model llava_onevision_with_kvcache \
-    --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.2 \
-    --tasks chartqa  \
-    --batch_size 1 \
-    --log_samples \
-    --log_samples_suffix chartqa \
-    --output_path ./logs/chartqa/
+# python3 -m accelerate.commands.launch \
+#     --num_processes=4 \
+#     -m lmms_eval \
+#     --model llava_onevision_with_kvcache \
+#     --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.01 \
+#     --tasks chartqa  \
+#     --batch_size 1 \
+#     --log_samples \
+#     --log_samples_suffix chartqa \
+#     --output_path ./logs/chartqa/
+# python3 -m accelerate.commands.launch \
+#     --num_processes=4 \
+#     -m lmms_eval \
+#     --model llava_onevision_with_kvcache \
+#     --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.05 \
+#     --tasks chartqa  \
+#     --batch_size 1 \
+#     --log_samples \
+#     --log_samples_suffix chartqa \
+#     --output_path ./logs/chartqa/
+# python3 -m accelerate.commands.launch \
+#     --num_processes=4 \
+#     -m lmms_eval \
+#     --model llava_onevision_with_kvcache \
+#     --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.1 \
+#     --tasks chartqa  \
+#     --batch_size 1 \
+#     --log_samples \
+#     --log_samples_suffix chartqa \
+#     --output_path ./logs/chartqa/
+# python3 -m accelerate.commands.launch \
+#     --num_processes=4 \
+#     -m lmms_eval \
+#     --model llava_onevision_with_kvcache \
+#     --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.2 \
+#     --tasks chartqa  \
+#     --batch_size 1 \
+#     --log_samples \
+#     --log_samples_suffix chartqa \
+#     --output_path ./logs/chartqa/
 # python3 -m accelerate.commands.launch \
 #     --num_processes=4 \
 #     -m lmms_eval \
@@ -60,6 +60,56 @@ python3 -m accelerate.commands.launch \
 #     --log_samples_suffix chartqa \
 #     --output_path ./logs/
 
+
+python3 -m accelerate.commands.launch \
+    --main_process_port=28175 \
+    --mixed_precision=bf16 \
+    --num_processes=4 \
+    -m lmms_eval \
+    --model llava_onevision_with_kvcache \
+    --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov \
+    --tasks chartqa  \
+    --batch_size 1 \
+    --log_samples \
+    --log_samples_suffix textvqa_val \
+    --output_path ./logs/chartqa/
+
+
+python3 -m accelerate.commands.launch \
+    --main_process_port=28175 \
+    --mixed_precision=bf16 \
+    --num_processes=4 \
+    -m lmms_eval \
+    --model llava_onevision_with_kvcache \
+    --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov \
+    --tasks textvqa_val  \
+    --batch_size 1 \
+    --log_samples \
+    --log_samples_suffix textvqa_val \
+    --output_path ./logs/textvqa_val/
+
+python3 -m accelerate.commands.launch \
+    --num_processes=4 \
+    --main_process_port=28175 \
+    --mixed_precision=bf16 \
+    -m lmms_eval \
+    --model llava_onevision_with_kvcache \
+    --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.01 \
+    --tasks textvqa_val  \
+    --batch_size 1 \
+    --log_samples \
+    --log_samples_suffix textvqa_val \
+    --output_path ./logs/textvqa_val/
+python3 -m accelerate.commands.launch \
+    --num_processes=4 \
+    -m lmms_eval \
+    --model llava_onevision_with_kvcache \
+    --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm,budgets=0.01 \
+    --tasks docvqa_test  \
+    --batch_size 1 \
+    --log_samples \
+    --log_samples_suffix docvqa \
+    --output_path ./logs/docvqa/
 # python3 -m accelerate.commands.launch \
 #     --num_processes=4 \
 #     -m lmms_eval \
@@ -69,7 +119,7 @@ python3 -m accelerate.commands.launch \
 #     --batch_size 1 \
 #     --log_samples \
 #     --log_samples_suffix textvqa_val \
-#     --output_path ./logs/
+#     --output_path ./logs/textvqa_val/
 
 # python3 -m accelerate.commands.launch \
 #     --num_processes=4 \
@@ -80,7 +130,7 @@ python3 -m accelerate.commands.launch \
 #     --batch_size 1 \
 #     --log_samples \
 #     --log_samples_suffix textvqa_val \
-#     --output_path ./logs/
+#     --output_path ./logs/textvqa_val/
 
 # python3 -m accelerate.commands.launch \
 #     --num_processes=4 \
@@ -91,7 +141,7 @@ python3 -m accelerate.commands.launch \
 #     --batch_size 1 \
 #     --log_samples \
 #     --log_samples_suffix textvqa_val \
-#     --output_path ./logs/
+#     --output_path ./logs/textvqa_val/
 
 
 # python3 -m accelerate.commands.launch \
@@ -103,16 +153,16 @@ python3 -m accelerate.commands.launch \
 #     --batch_size 1 \
 #     --log_samples \
 #     --log_samples_suffix textvqa_val \
-#     --output_path ./logs/
+#     --output_path ./logs/textvqa_val/
 
 
 # python3 -m accelerate.commands.launch \
 #     --num_processes=4 \
 #     -m lmms_eval \
 #     --model llava_onevision_with_kvcache \
-#     --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov,method=streamingllm\
+#     --model_args pretrained=/share/home/mhma/models/llava-onevision-qwen2-7b-ov\
 #     --tasks textvqa_val  \
 #     --batch_size 1 \
 #     --log_samples \
 #     --log_samples_suffix textvqa_val \
-#     --output_path ./logs/
+#     --output_path ./logs/textvqa_val/
