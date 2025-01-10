1aec83a6bedcd61f99fa99b63ed78d079d2b11cc
mhma <masn@bu.edu>
2025-01-10 23:08:54 +0800

    add prune methods

---
+++ pruner/utils.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+
+def apply_to_sample(f, sample):
+    if len(sample) == 0:
+        return {}
+
+    def _apply(x):
+        if torch.is_tensor(x):
+            return f(x)
+        elif isinstance(x, dict):
+            return {key: _apply(value) for key, value in x.items()}
+        elif isinstance(x, list):
+            return [_apply(x) for x in x]
+        else:
+            return x
+
+    return _apply(sample)
+
+
+def move_to_cuda(sample):
+    def _move_to_cuda(tensor):
+        return tensor.cuda()
+
+    return apply_to_sample(_move_to_cuda, sample)
+
+
+def prepare_sample(samples, cuda_enabled=True):
+    if cuda_enabled:
+        samples = move_to_cuda(samples)
+
+    return samples
+
+def get_module_recursive(base, module_to_process):
+    
+    if module_to_process == "":
+        return base
+    
+    splits = module_to_process.split(".")
+    now = splits.pop(0)
+    rest = ".".join(splits)
+    
+    base = getattr(base, now)
+
+    return get_module_recursive(base, rest)
+
+
+def find_layers(module, layers=[nn.Linear], name=''):
+
+    if type(module) in layers:
+        return {name: module}
+    res = {}
+    for name1, child in module.named_children():
+        res.update(find_layers(
+            child, layers=layers, name=name + '.' + name1 if name != '' else name1
+        ))
+    return res
+
+class WrappedGPT:
+
+    def __init__(self, layer, layer_id=0, layer_name="none"):
+        self.layer = layer
+        self.dev = self.layer.weight.device
+        self.rows = layer.weight.data.shape[0]
+        self.columns = layer.weight.data.shape[1]
+
+        self.scaler_row = torch.zeros((self.columns), device=self.dev)
+        self.nsamples = 0
+
+        self.layer_id = layer_id 
+        self.layer_name = layer_name
+
+    def add_batch(self, inp, out):  # inp: (batch, seq, hidden)
+        if len(inp.shape) == 2:
+            inp = inp.unsqueeze(0)
+        tmp = inp.shape[0]
+        if isinstance(self.layer, nn.Linear):
+            if len(inp.shape) == 3:
+                inp = inp.reshape((-1, inp.shape[-1]))
+            inp = inp.t()
+
+        self.scaler_row *= self.nsamples / (self.nsamples+tmp)
+        self.nsamples += tmp
+
+        inp = inp.type(torch.float32)
+        self.scaler_row += torch.norm(inp, p=2, dim=1) ** 2  / self.nsamples
+
+
+
+def loss_language(model, samples, cuda_enabled):
+    
+    samples = prepare_sample(samples, cuda_enabled=cuda_enabled)
+    loss_dict = model(**samples)
+    loss = loss_dict["loss"]
+
+    batch_len = len(samples["input_ids"])
+
+    return loss, batch_len
